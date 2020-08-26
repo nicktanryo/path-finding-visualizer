13d15c0fca859898ac32ffa71427737b08bb683a
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-08-26 22:19:21 +0700

    root app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import PathFinding from "./components";
+
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <div className="App">
+            <PathFinding />
+        </div>
+    );
 }
 
 export default App;
