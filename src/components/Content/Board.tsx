376808b39eaf9a6e7005aa3f7994ba2ca6af165b
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-08-28 21:55:38 +0700

    apply style and algorithm to the board

---
+++ src/components/Content/Board.tsx
@@ -0,0 +1,115 @@
+import React, { ReactElement, useState, useContext } from "react";
+import "./Board.css";
+
+// MUI --------------------------------------------------------------
+import { Theme, makeStyles } from "@material-ui/core";
+import ControlCameraIcon from "@material-ui/icons/ControlCamera";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+
+// parameters -------------------------------------------------------
+import { CELL_SIZE } from "../../parameter/board";
+
+// utilities --------------------------------------------------------
+import { IBoard } from "../../utilities/board";
+import { ICell } from "../../utilities/cell";
+import {
+    mouseEvent,
+    handleMouseDown,
+    handleMouseEnter,
+    handleMouseUp,
+} from "../../utilities/mouseEvents";
+
+//context -----------------------------------------------------------
+import { BoardContext, IBoardContext } from "../index";
+
+const useStyles = makeStyles((theme: Theme) => ({
+    row: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    cell: {
+        width: `${CELL_SIZE}px`,
+        height: `${CELL_SIZE}px`,
+        border: ".5px solid lightblue",
+    },
+    svg: {
+        width: "100%",
+        height: "100%",
+    },
+}));
+
+function Board(): ReactElement {
+    const classes = useStyles();
+
+    const boardContext = useContext<IBoardContext>(BoardContext);
+    const { board, setBoard: SetBoard, setSTART, setTARGET } = boardContext;
+
+    const [mouseDown, setMouseDown] = useState<boolean>(false);
+    const [holdingPiece, setHoldingPiece] = useState<string | null>(null);
+    const [prevPiece, setPrevPiece] = useState<Array<number>>([-1, -1]);
+
+    return (
+        <>
+            {board.map((row: Array<ICell>, indexI: number) => {
+                return (
+                    <div
+                        key={`row-${indexI}`}
+                        id={`row-${indexI}`}
+                        className={classes.row}
+                    >
+                        {row.map((cell: ICell, indexJ: number) => {
+                            let setBoard = SetBoard as React.Dispatch<
+                                React.SetStateAction<IBoard>
+                            >;
+                            let params: mouseEvent = {
+                                i: indexI,
+                                j: indexJ,
+                                board,
+                                setBoard,
+                                mouseDown,
+                                setMouseDown,
+                                holdingPiece,
+                                setHoldingPiece,
+                                prevPiece,
+                                setPrevPiece,
+                                setSTART,
+                                setTARGET,
+                            };
+                            return (
+                                <div
+                                    key={`cell-${indexI}-${indexJ}`}
+                                    id={`cell-${indexI}-${indexJ}`}
+                                    className={classes.cell}
+                                    onMouseDown={() => handleMouseDown(params)}
+                                    onMouseUp={() => handleMouseUp(params)}
+                                    onMouseEnter={() =>
+                                        handleMouseEnter(params)
+                                    }
+                                >
+                                    <div
+                                        className={
+                                            cell.isWall ? "background wall" : ""
+                                        }
+                                    >
+                                        {cell.isStart ? (
+                                            <KeyboardArrowRightIcon
+                                                className={classes.svg}
+                                            />
+                                        ) : cell.isTarget ? (
+                                            <ControlCameraIcon
+                                                className={classes.svg}
+                                            />
+                                        ) : null}
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
+        </>
+    );
+}
+
+export default Board;
