3482e61602373aa0f23ba99b5a5af8118111301e
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-08-28 21:54:18 +0700

    apply inheritance to interface

--- src/parameter/algorithm.ts
+++ src/parameter/algorithm.ts
@@ -1,6 +1,23 @@
-const ALGORITHM = {
+import { Dijkstra } from "../algorithms/path/dijkstra";
+import { IDataString, IDataFunction, func } from ".";
+
+interface IALGORITHM_OPTIONS extends IDataString {
+    DIJKSTRA: string;
+    ASTAR: string;
+}
+
+interface IALGORITHM extends IDataFunction {
+    DIJKSTRA: func;
+    ASTAR: func;
+}
+
+const ALGORITHM_OPTIONS: IALGORITHM_OPTIONS = {
     DIJKSTRA: "DIJKSTRA",
     ASTAR: "ASTAR",
 };
+const ALGORITHM: IALGORITHM = {
+    DIJKSTRA: Dijkstra as func,
+    ASTAR: Dijkstra as func,
+};
 
-export { ALGORITHM };
+export { ALGORITHM_OPTIONS, ALGORITHM };
--- src/parameter/speed.ts
+++ src/parameter/speed.ts
@@ -1,4 +1,12 @@
-const SPEED = {
+import { IDataNumber } from ".";
+
+interface ISPEED extends IDataNumber {
+    SLOW: number;
+    NORMAL: number;
+    FAST: number;
+}
+
+const SPEED: ISPEED = {
     SLOW: 25,
     NORMAL: 15,
     FAST: 10,
